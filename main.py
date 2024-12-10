45e35af8904593e36a168f0f1219a5f2b18eeca4
Иван Титов <titov_ivan@list.ru>
2024-12-10 22:23:49 +0500

    добавлена обработка кнопки генерации

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Slot
 
 from ui_mainwindow import Ui_MainWindow
+from file_create import generate_pdf
 
 
 class MainWindow(QMainWindow):
@@ -10,6 +12,12 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.generatePDF.clicked.connect(self.clic_generate)
+
+    def clic_generate(self):
+        date = {'name': 'III ii ii', 'date': [['10-31-31', 'Дифтерия']]}
+        generate_pdf(date)
+        print(1)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
